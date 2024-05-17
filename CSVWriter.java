51a9e22f6a81bb0c996d0573a39e208117455833
AbdulRahmanJainunTI22A <160212489+AbdulRahmanJainunTI22A@users.noreply.github.com>
2024-05-17 15:56:31 +0700

    Add files via upload

--- CSVWriter.java
+++ CSVWriter.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -13,20 +14,26 @@ import java.io.IOException;
 public class CSVWriter {
     public static void main(String[] args) {
         String csvFile = "C:\\Users\\kompoter 17\\Documents\\new_students.csv";
-        String[] data = {
-                "4, David, 23",
-                "5, Eva, 22",
-                "6, Ferdi, 21"
-        };
-
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFile))) {
-            for (String line : data) {
-                bw.write(line);
+        
+        Scanner sc = new Scanner(System.in);
+        boolean addData = true;            
+                
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFile))){
+            while (addData){
+                System.out.print("Masukkan data [NIM,NAMA,UMUR,PRODI] : ");
+                String data = sc.nextLine();
+                
+                bw.write(data);
                 bw.newLine();
+                
+                System.out.print("Apakah Ingin Menambahkan lagi? (y/n): ");
+                String response = sc.nextLine();
+                if(!response.equalsIgnoreCase("y")) {
+                    addData = false;
+                }
             }
-        } catch (IOException e) {
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
 }
-    
